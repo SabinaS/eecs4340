e54735c13de1b6d20b0637ed06847c3d79e80c08
Chae Jubb <ecj2122@columbia.edu>
2014-11-17 23:11:12 -0500

    ifc compiles now

--- mfe_ifc.sv
+++ mfe_ifc.sv
@@ -1,6 +1,6 @@
 interface mfe_ifc(input bit clk);
 
-	logic               clk, rst;
+	logic               rst;
 	logic [6:0]					status_leds;
 
 	logic								spi_clk;
@@ -17,10 +17,8 @@ interface mfe_ifc(input bit clk);
 
 	// note that the outputs and inputs are reversed from the dut
 	clocking cb @(posedge clk);
-		output rst, dat_spi_card_i, 
-		key_spi_card_i, out_spi_card_i,
-		input status_leds, spi_clk, dat_spi_card_o,
-		key_spi_card_o, out_spi_card_o, ps2_clk,
+		output rst, dat_spi_card_i, key_spi_card_i, out_spi_card_i;
+		input status_leds, spi_clk, dat_spi_card_o, key_spi_card_o, out_spi_card_o, ps2_clk;
 		inout  ps2_dat;
 	endclocking
 
@@ -29,8 +27,7 @@ interface mfe_ifc(input bit clk);
 	modport dut (
 		input clk, rst,dat_spi_card_i,key_spi_card_i,out_spi_card_i,
 		output status_leds, spi_clk,dat_spi_card_o,
-		output [2:0]        key_spi_card_o;
-		out_spi_card_o,ps2_clk
+		output key_spi_card_o, out_spi_card_o,ps2_clk,
 		inout ps2_dat
 	);
 endinterface

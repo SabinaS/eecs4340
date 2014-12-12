da382ed879cce95c711979cd23822b573c97a281
Tim Paine <t.paine154@gmail.com>
2014-12-12 11:32:58 -0500

    gcalc updates.

--- md5/md5_gcalc.sv
+++ md5/md5_gcalc.sv
@@ -5,10 +5,10 @@ module md5_gcalc (
     output logic [3:0] g
 );
 
-logic  doshift = 'b0;
-logic  sub = 'b0;
-logic   [1:0] shiftby = 'b0;
-logic   [2:0] addon = 'b0;
+logic  doshift;
+logic  sub;
+logic   [1:0] shiftby;
+logic   [2:0] addon;
 
 logic  [3:0] shift_res = (doshift) ? i[3:0] << shiftby : 4'b0;
 logic [3:0] mult_res = (sub) ? shift_res - i[3:0] : shift_res + i[3:0];
@@ -19,34 +19,34 @@ always_comb begin
         // 0 <= i <= 15
         2'b00: begin
             // g = i
-            doshift <= 1'b0;
-            sub <= 1'b0;
-            shiftby <= 2'b0;
-            addon <= 3'd0;
+            doshift = 1'b0;
+            sub = 1'b0;
+            shiftby = 2'b0;
+            addon = 3'd0;
         end
         // 16 <= i <= 31
         2'b01: begin
             // g = 5 * i + 1
-            doshift <= 1'b1;
-            sub <= 1'b0;
-            shiftby <= 2'b10;
-            addon <= 3'd1;
+            doshift = 1'b1;
+            sub = 1'b0;
+            shiftby = 2'b10;
+            addon = 3'd1;
         end
         // 32 <= i <= 47
         2'b10: begin
             // g = 3 * i + 5
-            doshift <= 1'b1;
-            sub <= 1'b0;
-            shiftby <= 2'b01;
-            addon <= 3'd5;
+            doshift = 1'b1;
+            sub = 1'b0;
+            shiftby = 2'b01;
+            addon = 3'd5;
         end
         // 48 <= i <= 63
         2'b11: begin
             // g = 7 * i
-            doshift <= 1'b1;
-            sub <= 1'b1;
-            shiftby <= 2'b11;
-            addon <= 3'd0;
+            doshift = 1'b1;
+            sub = 1'b1;
+            shiftby = 2'b11;
+            addon = 3'd0;
         end
     endcase
 end

3b59114c3d8a091e2f2388293e4078e0e196caec
Tim Paine <t.paine154@gmail.com>
2014-12-15 18:52:38 -0500

    Modify md5 to only process 1 md5 instead of 2.

--- md5/md5.sv
+++ md5/md5.sv
@@ -2,75 +2,74 @@
 
 module md5 (
     input logic clk,
-    input logic [1:0] reset,
-    input logic [1:0] start,
+    input logic reset,
+    input logic start,
 
     input logic write,
     input logic [31:0] writedata,
     input logic [4:0]  writeaddr,
 
     output logic [127:0] digest0,
-    output logic [127:0] digest1,
 
     output logic [1:0] done
 );
 
-logic [1:0] m_write;
-assign m_write[0] = write && !writeaddr[4];
-assign m_write[1] = write && writeaddr[4];
+logic m_write;
+assign m_write = write && !writeaddr[4];
 
-logic [5:0] cc_iaddr [1:0];
-logic [3:0] cc_gaddr [1:0];
+logic [5:0] cc_iaddr;
+logic [3:0] cc_gaddr;
 
-logic [127:0] cc_digest [1:0];
-assign digest0 = cc_digest[0];
-assign digest1 = cc_digest[1];
+logic [127:0] cc_digest;
+assign digest0 = cc_digest;
 
-logic [31:0] cc_mdata [1:0];
-logic [4:0]  cc_sdata [1:0];
-logic [31:0] cc_kdata [1:0];
+logic [31:0] cc_mdata;
+logic [4:0]  cc_sdata;
+logic [31:0] cc_kdata;
 
-genvar i;
-generate
-    for (i = 0; i < 2; i = i + 1) begin : mccgen
-        md5_chunk_cruncher cc (
-            .clk (clk),
-            .reset (reset[i]),
-            .start (start[i]),
-            .done (done[i]),
-            .digest (cc_digest[i]),
-            .iaddr (cc_iaddr[i]),
-            .kdata (cc_kdata[i]),
-            .sdata (cc_sdata[i]),
-            .gaddr (cc_gaddr[i]),
-            .mdata (cc_mdata[i])
-        );
+md5_chunk_cruncher cc (
+    .clk (clk),
+    .reset (reset),
+    .start (start),
+    .done (done),
+    .digest (cc_digest),
+    .iaddr (cc_iaddr),
+    .kdata (cc_kdata),
+    .sdata (cc_sdata),
+    .gaddr (cc_gaddr),
+    .mdata (cc_mdata)
+);
 
-        mdataram mram (
-            .clock (clk),
-            .data (writedata),
-            .wraddress (writeaddr[3:0]),
-            .wren (m_write[i]),
-            .rdaddress (cc_gaddr[i]),
-            .q (cc_mdata[i])
-        );
-    end
-endgenerate
+mdataram mram (
+    .clock (clk),
+    .data (writedata),
+    .wraddress (writeaddr[3:0]),
+    .wren (m_write),
+    .rdaddress (cc_gaddr),
+    .q (cc_mdata)
+);
+
+
+/* don't use second channel of memory */
+logic [5:0] cc_iaddr_unused;
+logic [3:0] cc_gaddr_unused;
+logic [4:0]  cc_sdata_unused;
+logic [31:0] cc_kdata_unused;
 
 sdatarom srom (
     .clock (clk),
-    .address_a (cc_iaddr[0]),
-    .address_b (cc_iaddr[1]),
-    .q_a (cc_sdata[0]),
-    .q_b (cc_sdata[1])
+    .address_a (cc_iaddr),
+    .address_b (cc_iaddr_unused), //unused
+    .q_a (cc_sdata),
+    .q_b (cc_sdata_unused) //unused
 );
 
 kdatarom krom (
     .clock (clk),
-    .address_a (cc_iaddr[0]),
-    .address_b (cc_iaddr[1]),
-    .q_a (cc_kdata[0]),
-    .q_b (cc_kdata[1])
+    .address_a (cc_iaddr),
+    .address_b (cc_iaddr_unused), //unused
+    .q_a (cc_kdata),
+    .q_b (cc_kdata_unused) //unused
 );
 
 endmodule

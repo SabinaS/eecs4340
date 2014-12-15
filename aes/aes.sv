3dcfbe570f03735211b598e12c6990103b7e6c43
Tim Paine <t.paine154@gmail.com>
2014-12-15 18:53:07 -0500

    Add aes wrapper, will need to deal with stalls (clock gate if necessary)

---
+++ aes/aes.sv
@@ -0,0 +1,17 @@
+//inv_aespipe wrapper
+
+module aes(
+	clk, rst, stall,
+	aes_in, key, data_out
+);
+
+	input clk, rst, stall;
+	input [127:0] key;
+	input [127:0] aes_in;
+	output [127:0] data_out;//38 cycles later
+
+	inv_aespipe aes_inst(.*);
+	
+
+
+

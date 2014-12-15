a3ccb81304478dffb924a5a9164f9447b7a6c39e
Sabina Smajlaj <ss3912@columbia.edu>
2014-12-15 00:34:50 -0500

    Added initial folder for ps2

---
+++ ps2/ps2_ifc.sv
@@ -0,0 +1,17 @@
+'timescale 1ns/1ns
+
+interface ps2_ifc(input bit clk):
+	
+	logic rst;
+	
+	clocking cb @(posedge clk);
+		/* ToDo */
+	endclocking 
+
+	modport bench (clocking cb);
+
+	modport dut(
+		/* ToDo */
+	);
+
+endinterface 

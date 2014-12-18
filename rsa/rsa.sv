36e0736363a8885c96733762e9c8e0a0f29ca46a
Tim Paine <t.paine154@gmail.com>
2014-12-17 23:53:18 -0500

    RSA converted to 128 bit interconnects.

--- rsa/rsa.sv
+++ rsa/rsa.sv
@@ -12,12 +12,12 @@ module rsa(
 
 	input logic aes_valid_i, rsa_valid_i;
 	output logic aes_ready_o, rsa_ready_o;
-	input logic [31:0] aes_data_i, rsa_data_i;
+	input logic [127:0] aes_data_i, rsa_data_i;
 
 	input logic [7:0] ps2_data_i; 
 	input logic ps2_done, ps2_reset, ps2_valid_i;
 
-	output logic [31:0] out_data_o;
+	output logic [127:0] out_data_o;
 	output logic out_valid_o;
 	input logic out_ready_i;
 
@@ -109,7 +109,7 @@ module rsa(
 		end else if(!stall) begin
 			case(state)
 				3'b000: begin //get all RSA stuff
-					if(count==268) begin //(4096+4096+384)/32
+					if(count==66) begin //(4096+4096+256)/128
 						state <= 3'b001;
 						count <= 0;
 						rsa_ready_o<=1'b1;
@@ -151,7 +151,7 @@ module rsa(
 
 				3'b011: begin //decrypt 4096 RSA exp and 4096 RSA mod
 					//start_aes_decrypt <= 1'b0;
-					if(count == 101) begin //64-101 are good 
+					if(count == 101) begin //64-101 are good  TODO FIX
 						state <= 3'b100;
 						count <= 0;
 						aes_ready_o <= 1'b1;
@@ -161,15 +161,16 @@ module rsa(
 				end
 
 				3'b100: begin //buffer encrypted AES keys
-					if(count == 8) begin
+					//if(count == 8) begin //only takes 1 cycle now
+					if(aes_valid_i) begin 
 						state <= 3'b101;
 						count <= 0;
 						aes_ready_o <= 1'b0; // one cycle too late??
 						start_rsa_decrypt <= 1'b1;
 					end else begin
-						if(aes_valid_i) begin
-							count <= count + 1;
-						end
+					//	if(aes_valid_i) begin
+					//		count <= count + 1;
+					//	end
 					end
 				end
 
@@ -183,13 +184,14 @@ module rsa(
 				end
 
 				3'b110: begin /* send to AES data module */
-					if(count == 8) begin
+					//if(count == 8) begin
+					if(out_ready_i) begin
 						state <= 3'b111;
 						count <= 0;
 					end else begin 
-						if(out_ready_i) begin
-							count <= count + 1;
-						end
+					//	if(out_ready_i) begin
+					//		count <= count + 1;
+					//	end
 					end
 				end
 
@@ -213,7 +215,7 @@ module rsa(
 				3'b000: begin 
 					/* buffer encrypted RSA stuff */
 					if(rsa_valid_i==1'b1) begin
-						input_buff[count*32+:32] <= rsa_data_i;
+						input_buff[count*128+:128] <= rsa_data_i;
 					end
 				end
 				
@@ -247,7 +249,7 @@ module rsa(
 				3'b100: begin //buffer encrypted AES keys
 					/* Encrypted AES Key */
 					if(aes_valid_i) begin
-						aes[(32*count)+:32] <= aes_data_i;
+						aes <= aes_data_i;
 					end
 				end
 				
@@ -259,7 +261,7 @@ module rsa(
 				3'b110: begin /* send to AES data module */
 					/* Output Key */
 					if(out_ready_i) begin
-						out_data_o <= aes_d[(count*32)+:32];// convert to chunked
+						out_data_o <= aes_d;
 						out_valid_o <= 1'b1;
 					end
 				end

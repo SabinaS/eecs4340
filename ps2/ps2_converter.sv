143f1220a506b8b3794cb9e1a5392785184eba65
Sabina Smajlaj <sabinasmajlaj@gmail.com>
2014-12-19 13:03:13 -0500

    ...

--- ps2/ps2_converter.sv
+++ ps2/ps2_converter.sv
@@ -246,11 +246,11 @@ always_ff @(posedge clk) begin
                 /* should have default??  */
 
 			end else begin
-				case (ps2_code ==  8'h29 ) begin ascii <= 0x20; endcase
-                case (ps2_code ==  8'h66 ) begin ascii <= 0x08; endcase
-                case (ps2_code ==  8'h0D ) begin ascii <= 0x09; endcase
-                case (ps2_code ==  8'h5A ) begin ascii <= 0x0D; endcase
-                case (ps2_code ==  8'h76 ) begin ascii <= 0x1B; endcase
+				case (ps2_code ==  8'h29 ) begin ascii <= 8'h20; endcase
+                case (ps2_code ==  8'h66 ) begin ascii <= 8'h08; endcase
+                case (ps2_code ==  8'h0D ) begin ascii <= 8'h09; endcase
+                case (ps2_code ==  8'h5A ) begin ascii <= 8'h0D; endcase
+                case (ps2_code ==  8'h76 ) begin ascii <= 8'h1B; endcase
                 case (ps2_code ==  8'h71 ) begin 
                 	if(e0_code == '1) begin      
      					ascii <= 8'h7F;             
@@ -261,105 +261,105 @@ always_ff @(posedge clk) begin
                 if( (shift_r == '0 && shift_l == '0 && caps_lock == '0) 
                 		|| ((shift_r == '1 || shift_l == '1) 
                 		&& caps_lock == '1) ) begin
-                	case (ps2_code ==  8'h1C) ascii <= 0x61; endcase
-					case (ps2_code ==  8'h32) ascii <= 0x62; endcase
-					case (ps2_code ==  8'h21) ascii <= 0x63; endcase
-					case (ps2_code ==  8'h23) ascii <= 0x64; endcase
-					case (ps2_code ==  8'h24) ascii <= 0x65; endcase
-					case (ps2_code ==  8'h2B) ascii <= 0x66; endcase
-					case (ps2_code ==  8'h34) ascii <= 0x67; endcase
-					case (ps2_code ==  8'h33) ascii <= 0x68; endcase
-					case (ps2_code ==  8'h43) ascii <= 0x69; endcase
-					case (ps2_code ==  8'h3B) ascii <= 0x6A; endcase
-					case (ps2_code ==  8'h42) ascii <= 0x6B; endcase
-					case (ps2_code ==  8'h4B) ascii <= 0x6C; endcase
-					case (ps2_code ==  8'h3A) ascii <= 0x6D; endcase
-					case (ps2_code ==  8'h31) ascii <= 0x6E; endcase
-					case (ps2_code ==  8'h44) ascii <= 0x6F; endcase
-					case (ps2_code ==  8'h4D) ascii <= 0x70; endcase
-					case (ps2_code ==  8'h15) ascii <= 0x71; endcase
-					case (ps2_code ==  8'h2D) ascii <= 0x72; endcase
-					case (ps2_code ==  8'h1B) ascii <= 0x73; endcase
-					case (ps2_code ==  8'h2C) ascii <= 0x74; endcase
-					case (ps2_code ==  8'h3C) ascii <= 0x75; endcase
-					case (ps2_code ==  8'h2A) ascii <= 0x76; endcase
-					case (ps2_code ==  8'h1D) ascii <= 0x77; endcase
-					case (ps2_code ==  8'h22) ascii <= 0x78; endcase
-					case (ps2_code ==  8'h35) ascii <= 0x79; endcase
-					case (ps2_code ==  8'h1A) ascii <= 0x7A; endcase
+                	case (ps2_code ==  8'h1C) ascii <= 8'h61; endcase
+					case (ps2_code ==  8'h32) ascii <= 8'h62; endcase
+					case (ps2_code ==  8'h21) ascii <= 8'h63; endcase
+					case (ps2_code ==  8'h23) ascii <= 8'h64; endcase
+					case (ps2_code ==  8'h24) ascii <= 8'h65; endcase
+					case (ps2_code ==  8'h2B) ascii <= 8'h66; endcase
+					case (ps2_code ==  8'h34) ascii <= 8'h67; endcase
+					case (ps2_code ==  8'h33) ascii <= 8'h68; endcase
+					case (ps2_code ==  8'h43) ascii <= 8'h69; endcase
+					case (ps2_code ==  8'h3B) ascii <= 8'h6A; endcase
+					case (ps2_code ==  8'h42) ascii <= 8'h6B; endcase
+					case (ps2_code ==  8'h4B) ascii <= 8'h6C; endcase
+					case (ps2_code ==  8'h3A) ascii <= 8'h6D; endcase
+					case (ps2_code ==  8'h31) ascii <= 8'h6E; endcase
+					case (ps2_code ==  8'h44) ascii <= 8'h6F; endcase
+					case (ps2_code ==  8'h4D) ascii <= 8'h70; endcase
+					case (ps2_code ==  8'h15) ascii <= 8'h71; endcase
+					case (ps2_code ==  8'h2D) ascii <= 8'h72; endcase
+					case (ps2_code ==  8'h1B) ascii <= 8'h73; endcase
+					case (ps2_code ==  8'h2C) ascii <= 8'h74; endcase
+					case (ps2_code ==  8'h3C) ascii <= 8'h75; endcase
+					case (ps2_code ==  8'h2A) ascii <= 8'h76; endcase
+					case (ps2_code ==  8'h1D) ascii <= 8'h77; endcase
+					case (ps2_code ==  8'h22) ascii <= 8'h78; endcase
+					case (ps2_code ==  8'h35) ascii <= 8'h79; endcase
+					case (ps2_code ==  8'h1A) ascii <= 8'h7A; endcase
                 end else begin
-                	case (ps2_code ==  8'h1C) ascii <= 0x41; endcase
-					case (ps2_code ==  8'h32) ascii <= 0x42; endcase
-					case (ps2_code ==  8'h21) ascii <= 0x43; endcase
-					case (ps2_code ==  8'h23) ascii <= 0x44; endcase
-					case (ps2_code ==  8'h24) ascii <= 0x45; endcase
-					case (ps2_code ==  8'h2B) ascii <= 0x46; endcase
-					case (ps2_code ==  8'h34) ascii <= 0x47; endcase
-					case (ps2_code ==  8'h33) ascii <= 0x48; endcase
-					case (ps2_code ==  8'h43) ascii <= 0x49; endcase
-					case (ps2_code ==  8'h3B) ascii <= 0x4A; endcase
-					case (ps2_code ==  8'h42) ascii <= 0x4B; endcase
-					case (ps2_code ==  8'h4B) ascii <= 0x4C; endcase
-					case (ps2_code ==  8'h3A) ascii <= 0x4D; endcase
-					case (ps2_code ==  8'h31) ascii <= 0x4E; endcase
-					case (ps2_code ==  8'h44) ascii <= 0x4F; endcase
-					case (ps2_code ==  8'h4D) ascii <= 0x50; endcase
-					case (ps2_code ==  8'h15) ascii <= 0x51; endcase
-					case (ps2_code ==  8'h2D) ascii <= 0x52; endcase
-					case (ps2_code ==  8'h1B) ascii <= 0x53; endcase
-					case (ps2_code ==  8'h2C) ascii <= 0x54; endcase 
-					case (ps2_code ==  8'h3C) ascii <= 0x55; endcase 
-					case (ps2_code ==  8'h2A) ascii <= 0x56; endcase 
-					case (ps2_code ==  8'h1D) ascii <= 0x57; endcase
-					case (ps2_code ==  8'h22) ascii <= 0x58; endcase
-					case (ps2_code ==  8'h35) ascii <= 0x59; endcase
-					case (ps2_code ==  8'h1A) ascii <= 0x5A; endcase
+                	case (ps2_code ==  8'h1C) ascii <= 8'h41; endcase
+					case (ps2_code ==  8'h32) ascii <= 8'h42; endcase
+					case (ps2_code ==  8'h21) ascii <= 8'h43; endcase
+					case (ps2_code ==  8'h23) ascii <= 8'h44; endcase
+					case (ps2_code ==  8'h24) ascii <= 8'h45; endcase
+					case (ps2_code ==  8'h2B) ascii <= 8'h46; endcase
+					case (ps2_code ==  8'h34) ascii <= 8'h47; endcase
+					case (ps2_code ==  8'h33) ascii <= 8'h48; endcase
+					case (ps2_code ==  8'h43) ascii <= 8'h49; endcase
+					case (ps2_code ==  8'h3B) ascii <= 8'h4A; endcase
+					case (ps2_code ==  8'h42) ascii <= 8'h4B; endcase
+					case (ps2_code ==  8'h4B) ascii <= 8'h4C; endcase
+					case (ps2_code ==  8'h3A) ascii <= 8'h4D; endcase
+					case (ps2_code ==  8'h31) ascii <= 8'h4E; endcase
+					case (ps2_code ==  8'h44) ascii <= 8'h4F; endcase
+					case (ps2_code ==  8'h4D) ascii <= 8'h50; endcase
+					case (ps2_code ==  8'h15) ascii <= 8'h51; endcase
+					case (ps2_code ==  8'h2D) ascii <= 8'h52; endcase
+					case (ps2_code ==  8'h1B) ascii <= 8'h53; endcase
+					case (ps2_code ==  8'h2C) ascii <= 8'h54; endcase 
+					case (ps2_code ==  8'h3C) ascii <= 8'h55; endcase 
+					case (ps2_code ==  8'h2A) ascii <= 8'h56; endcase 
+					case (ps2_code ==  8'h1D) ascii <= 8'h57; endcase
+					case (ps2_code ==  8'h22) ascii <= 8'h58; endcase
+					case (ps2_code ==  8'h35) ascii <= 8'h59; endcase
+					case (ps2_code ==  8'h1A) ascii <= 8'h5A; endcase
                 end
 
                 if(shift_l == '1 || shift_r == '1) begin
-                	case (ps2_code ==  8'h16) ascii <= 0x21; endcase
-					case (ps2_code ==  8'h52) ascii <= 0x22; endcase
-					case (ps2_code ==  8'h26) ascii <= 0x23; endcase
-					case (ps2_code ==  8'h25) ascii <= 0x24; endcase
-					case (ps2_code ==  8'h2E) ascii <= 0x25; endcase
-					case (ps2_code ==  8'h3D) ascii <= 0x26; endcase              
-					case (ps2_code ==  8'h46) ascii <= 0x28; endcase
-					case (ps2_code ==  8'h45) ascii <= 0x29; endcase
-					case (ps2_code ==  8'h3E) ascii <= 0x2A; endcase
-					case (ps2_code ==  8'h55) ascii <= 0x2B; endcase
-					case (ps2_code ==  8'h4C) ascii <= 0x3A; endcase
-					case (ps2_code ==  8'h41) ascii <= 0x3C; endcase
-					case (ps2_code ==  8'h49) ascii <= 0x3E; endcase
-					case (ps2_code ==  8'h4A) ascii <= 0x3F; endcase
-					case (ps2_code ==  8'h1E) ascii <= 0x40; endcase
-					case (ps2_code ==  8'h36) ascii <= 0x5E; endcase
-					case (ps2_code ==  8'h4E) ascii <= 0x5F; endcase
-					case (ps2_code ==  8'h54) ascii <= 0x7B; endcase
-					case (ps2_code ==  8'h5D) ascii <= 0x7C; endcase
-					case (ps2_code ==  8'h5B) ascii <= 0x7D; endcase
-					case (ps2_code ==  8'h0E) ascii <= 0x7E; endcase
+                	case (ps2_code ==  8'h16) ascii <= 8'h21; endcase
+					case (ps2_code ==  8'h52) ascii <= 8'h22; endcase
+					case (ps2_code ==  8'h26) ascii <= 8'h23; endcase
+					case (ps2_code ==  8'h25) ascii <= 8'h24; endcase
+					case (ps2_code ==  8'h2E) ascii <= 8'h25; endcase
+					case (ps2_code ==  8'h3D) ascii <= 8'h26; endcase              
+					case (ps2_code ==  8'h46) ascii <= 8'h28; endcase
+					case (ps2_code ==  8'h45) ascii <= 8'h29; endcase
+					case (ps2_code ==  8'h3E) ascii <= 8'h2A; endcase
+					case (ps2_code ==  8'h55) ascii <= 8'h2B; endcase
+					case (ps2_code ==  8'h4C) ascii <= 8'h3A; endcase
+					case (ps2_code ==  8'h41) ascii <= 8'h3C; endcase
+					case (ps2_code ==  8'h49) ascii <= 8'h3E; endcase
+					case (ps2_code ==  8'h4A) ascii <= 8'h3F; endcase
+					case (ps2_code ==  8'h1E) ascii <= 8'h40; endcase
+					case (ps2_code ==  8'h36) ascii <= 8'h5E; endcase
+					case (ps2_code ==  8'h4E) ascii <= 8'h5F; endcase
+					case (ps2_code ==  8'h54) ascii <= 8'h7B; endcase
+					case (ps2_code ==  8'h5D) ascii <= 8'h7C; endcase
+					case (ps2_code ==  8'h5B) ascii <= 8'h7D; endcase
+					case (ps2_code ==  8'h0E) ascii <= 8'h7E; endcase
                 end else begin
-                	case (ps2_code ==  8'h45) ascii <= 0x30; endcase
-					case (ps2_code ==  8'h16) ascii <= 0x31; endcase
-					case (ps2_code ==  8'h1E) ascii <= 0x32; endcase
-					case (ps2_code ==  8'h26) ascii <= 0x33; endcase
-					case (ps2_code ==  8'h25) ascii <= 0x34; endcase
-					case (ps2_code ==  8'h2E) ascii <= 0x35; endcase
-					case (ps2_code ==  8'h36) ascii <= 0x36; endcase
-					case (ps2_code ==  8'h3D) ascii <= 0x37; endcase
-					case (ps2_code ==  8'h3E) ascii <= 0x38; endcase
-					case (ps2_code ==  8'h46) ascii <= 0x39; endcase
-					case (ps2_code ==  8'h52) ascii <= 0x27; endcase
-					case (ps2_code ==  8'h41) ascii <= 0x2C; endcase
-					case (ps2_code ==  8'h4E) ascii <= 0x2D; endcase
-					case (ps2_code ==  8'h49) ascii <= 0x2E; endcase
-					case (ps2_code ==  8'h4A) ascii <= 0x2F; endcase
-					case (ps2_code ==  8'h4C) ascii <= 0x3B; endcase
-					case (ps2_code ==  8'h55) ascii <= 0x3D; endcase
-					case (ps2_code ==  8'h54) ascii <= 0x5B; endcase
-					case (ps2_code ==  8'h5D) ascii <= 0x5C; endcase
-					case (ps2_code ==  8'h5B) ascii <= 0x5D; endcase
-					case (ps2_code ==  8'h0E) ascii <= 0x60; endcase
+                	case (ps2_code ==  8'h45) ascii <= 8'h30; endcase
+					case (ps2_code ==  8'h16) ascii <= 8'h31; endcase
+					case (ps2_code ==  8'h1E) ascii <= 8'h32; endcase
+					case (ps2_code ==  8'h26) ascii <= 8'h33; endcase
+					case (ps2_code ==  8'h25) ascii <= 8'h34; endcase
+					case (ps2_code ==  8'h2E) ascii <= 8'h35; endcase
+					case (ps2_code ==  8'h36) ascii <= 8'h36; endcase
+					case (ps2_code ==  8'h3D) ascii <= 8'h37; endcase
+					case (ps2_code ==  8'h3E) ascii <= 8'h38; endcase
+					case (ps2_code ==  8'h46) ascii <= 8'h39; endcase
+					case (ps2_code ==  8'h52) ascii <= 8'h27; endcase
+					case (ps2_code ==  8'h41) ascii <= 8'h2C; endcase
+					case (ps2_code ==  8'h4E) ascii <= 8'h2D; endcase
+					case (ps2_code ==  8'h49) ascii <= 8'h2E; endcase
+					case (ps2_code ==  8'h4A) ascii <= 8'h2F; endcase
+					case (ps2_code ==  8'h4C) ascii <= 8'h3B; endcase
+					case (ps2_code ==  8'h55) ascii <= 8'h3D; endcase
+					case (ps2_code ==  8'h54) ascii <= 8'h5B; endcase
+					case (ps2_code ==  8'h5D) ascii <= 8'h5C; endcase
+					case (ps2_code ==  8'h5B) ascii <= 8'h5D; endcase
+					case (ps2_code ==  8'h0E) ascii <= 8'h60; endcase
                 end
 			end
 

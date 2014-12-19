ccbd6ede258d8c6b75687cd6aa2aed9765966441
Chae Jubb <ecj2122@columbia.edu>
2014-12-19 05:54:53 -0500

    confident in TB implementation of modexp

--- rsa/modexp_tb.sv
+++ rsa/modexp_tb.sv
@@ -1,21 +1,24 @@
 class transaction;
 	// vars
-	rand bit reset;
 	rand logic [4095:0] exp, mod;
-	rand logic [127:0] key_i;
-	rand logic [127:0] key_o, intermediate;
+	rand logic [127:0] key_i; /* really padded with zeros */
+	logic [4095:0] key_o;
+	logic [8191:0] intermediate;
 
-	function void generate_data(bit rst);
+	function void generate_data();
 		int i;
 
-		reset = rst;
-		intermediate = exp[127] ? key_i : '1;
-
-		for(i = 126; i == 0; --i) begin
-			intermediate = intermediate * intermediate * (key_i[i] ? key_i : '1);
+		intermediate = '1;
+		for(i = 4095; i >= 0; --i) begin
+			if (exp[i] == 1) begin
+				intermediate = (key_i * intermediate) % mod;
+			end
+			if (i != 0) begin
+				intermediate = (intermediate * intermediate) % mod;
+			end
 		end
 
-		key_o = intermediate;
+		key_o = intermediate[4095:0];
 	endfunction
 
 	function bit verify_data(logic[127:0] recieved);
@@ -66,11 +69,11 @@ class testing_env;
 	endfunction
 
 	function bit get_reset();
-		return ((reset_rn % 1000) < reset_thresh);
+		return ((reset_rn % 10000) < reset_thresh);
 	endfunction
 
 	function bit get_stall();
-		return ((stall_rn % 1000) < stall_thresh);
+		return ((stall_rn % 10000) < stall_thresh);
 	endfunction
 
 endclass
@@ -84,14 +87,12 @@ program modexp_tb (modexp_ifc.bench ds);
 	int failures = 0;
 	bit reset;
 	bit should_initiate;
+	bit force_reset = '0;
 	initial begin
 		t = new();
 		v = new();
 		v.read_config("modexp_config.txt");
 		v.handle_reset();
-
-		// Drive inputs for next cycles
-		ds.cb.rst <= t.reset;
 
 		// run reset for a little bit
 		repeat(10) begin
@@ -106,7 +107,7 @@ program modexp_tb (modexp_ifc.bench ds);
 		// Iterate iter number of cycles
 		repeat (v.iter) begin
 			v.randomize();
-			if(v.get_reset()) begin
+			if(v.get_reset() || force_reset) begin
 				ds.cb.rst <= 1'b1;
 				$display("%t : %s \n", $realtime, "Driving Reset");
 				should_initiate <= '1; /* start data transfer next cycle */
@@ -114,11 +115,12 @@ program modexp_tb (modexp_ifc.bench ds);
 				ds.cb.rst <= 1'b0;
 				if (v.get_stall()) begin
 					/* not sure if I care */
-					//$display("%t : %s \n", $realtime, "Driving Stall");
+					$display("%t : %s \n", $realtime, "Driving Stall");
 				end else if (should_initiate) begin
+					ds.cb.stall<= '0;
 					$display("%t : %s \n", $realtime, "Driving Data");
 					t.randomize();
-					t.generate_data(v.get_reset());
+					t.generate_data();
 					ds.cb.mod <= t.mod;
 					ds.cb.exp <= t.exp;
 					ds.cb.data_i <= t.key_i;
@@ -131,19 +133,23 @@ program modexp_tb (modexp_ifc.bench ds);
 
 			@(ds.cb);
 
-			if(v.get_reset()) begin
+			if(v.get_reset() || force_reset) begin
 				$display("%t : %s \n", $realtime,
 					t.check_reset(ds.cb.done)
 					? "Pass-reset" : "Fail-reset");
 				v.handle_reset();
+				force_reset <= '0;
 			end else begin
 				if (v.get_stall()) begin
 					/* don't think I care */
-					//$display("%t : %s \n", $realtime, "Pass-stall");
+					$display("%t : %s \n", $realtime, "Pass-stall");
 				end else if (ds.cb.done) begin
-					$display("%t : %s \n", $realtime,
-						t.verify_data(ds.cb.data_o) ?
-						"Pass-data" : "Fail-data");
+					if (t.verify_data(ds.cb.data_o)) begin
+						$display("%t : %s \n", $realtime, "Pass-data");
+					end else begin
+						$display("\nFail-data: %d %d\n\n", ds.cb.data_o, t.key_o);
+					end
+					force_reset <= '1;
 				end
 			end
 

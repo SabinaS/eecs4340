e558134ace4adc9d0729a9461f167ba2864bed6f
Chae Jubb <ecj2122@columbia.edu>
2014-12-19 13:37:03 -0500

    modexp done [working]

--- rsa/modexp_tb.sv
+++ rsa/modexp_tb.sv
@@ -3,22 +3,17 @@ class transaction;
 	rand logic [4095:0] exp, mod;
 	rand logic [4095:0] key_i; /* really padded with zeros */
 	logic [4095:0] key_o;
-	logic [8191:0] intermediate;
+	logic [8195:0] intermediate;
 
 	function void generate_data();
 		int i;
 
-		/* DEBUGGING ONLY */
-    exp[4095:16] = '0;
-    mod[4095:16] = '0;
-    key_i[4095:16] = '0;
-
     key_i = key_i % mod;
 
-		intermediate = '1;
+		intermediate = 'b01;
 		for(i = 4095 /* length exp */; i >= 0; --i) begin
 			if (exp[i] == 1) begin
-				intermediate = (key_i * intermediate) % mod;
+				intermediate = (intermediate * key_i) % mod;
 			end
 			if (i != 0) begin
 				intermediate = (intermediate * intermediate) % mod;
@@ -154,7 +149,7 @@ program modexp_tb (modexp_ifc.bench ds);
 					if (t.verify_data(ds.cb.data_o)) begin
 						$display("%t : %s \n", $realtime, "Pass-data");
 					end else begin
-						$display("\nFail-data: %d %d::%d**%d mod %d\n\n",
+						$display("\nFail-data (Low bits): %d %d::%d**%d mod %d\n\n",
 							ds.cb.data_o[15:0], t.key_o[15:0], t.key_i[15:0], t.exp[15:0], t.mod[15:0]);
 					end
 					force_reset <= '1;

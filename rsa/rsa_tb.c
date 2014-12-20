005cce4e69c9f273ef421e1551535dc5485e6872
Chae Jubb <ecj2122@columbia.edu>
2014-12-20 15:58:26 -0500

    cleanup rsa tb for submission

--- rsa/rsa_tb.c
+++ rsa/rsa_tb.c
@@ -13,8 +13,6 @@
 
 #define MD5_OUT_SIZE 16
 
-char *hash;
-
 void md5hash(char *src, svBitPackedArrRef res, int in_len) {
 	int i;
 	char *src_cpy = malloc(in_len);
@@ -39,13 +37,6 @@ void md5hash(char *src, svBitPackedArrRef res, int in_len) {
 
 	EVP_MD_CTX_destroy(ctx);
 
-	for (i = 0; i < MD5_OUT_SIZE; ++i) {
-		printf("%02x ", ((char *)res_char)[i] & 0xff);
-	}
-
-	hash = malloc(MD5_OUT_SIZE);
-	memcpy(hash, res_char, MD5_OUT_SIZE);
-
 	/* byte ordering mess */
 	for (i = 0; i < MD5_OUT_SIZE / 2; ++i) {
 		char tmp = res_char[i];
@@ -54,17 +45,6 @@ void md5hash(char *src, svBitPackedArrRef res, int in_len) {
 	}
 
 	memcpy(res, res_char, MD5_OUT_SIZE);
-
-	printf("MD5::\n");
-	for (i = 0; i < in_len; ++i) {
-		printf("%02x ", ((char *)src)[i] & 0xff);
-	}
-	printf("\n");
-	for (i = 0; i < MD5_OUT_SIZE; ++i) {
-		printf("%02x ", ((char *)res)[i] & 0xff);
-	}
-	printf("\n");
-
 	free(res_char);
 }
 
@@ -75,8 +55,6 @@ void aes_encrypt(svBitPackedArrRef key, svBitPackedArrRef src,
 	memmove(key_char, key, in_len);
 	memmove(res_char, src, in_len);
 
-//	memmove(key_char, hash, in_len);
-	//memmove(res_char, hash, in_len);
 	int i;
 	for (i = 0; i < in_len / 2; ++i) {
 		char tmp = res_char[i];
@@ -92,12 +70,6 @@ void aes_encrypt(svBitPackedArrRef key, svBitPackedArrRef src,
 
 	/* in-place modification of res */
 	aes_encrypt_block(res_char, key_char);
-
-	printf("AES::\n");
-	for (i = 0; i < 16; ++i) {
-		printf("%02x ", res_char[i] & 0xff);
-	}
-	printf("\n");
 
 	for (i = 0; i < in_len / 2; ++i) {
 		char tmp = res_char[i];

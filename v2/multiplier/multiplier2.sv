5750724ec31c9a52d130100a6c670b2a931b4246
Tim Paine <t.paine154@gmail.com>
2015-02-11 14:18:55 -0500

    .

--- v2/multiplier/multiplier2.sv
+++ v2/multiplier/multiplier2.sv
@@ -15,16 +15,28 @@ module multiplier2(
 genvar i;
 generate
 for(i=0;i<4096;i=i+1) begin
-assign intermediate[4095+i:i] = (stage == i) ? ((a[i] == 1 ) ?  intermediate[4095+i:0+i] + a : intermediate[4095+i:i]) : intermediate[4095+i:i]; 
+	always_comb begin
+		if(stage == i && rst != 1) begin
+			intermediate[4095+i:i] = (a[i] == 1 ) ?  intermediate[4095+i:0+i] + a : intermediate[4095+i:i]; 
+		end
+	end
 end
 endgenerate 
+
+	always_comb begin 
+		if(rst==1) begin
+			intermediate<='b0;
+		end
+	end
+	
+
 
 
 	always_ff @(posedge clk) begin
 		if(rst) begin
 			stage <= 'b0;
 		end else begin
-                 	if(stage == 12'b111111111111) begin
+         	if(stage == 12'b111111111111) begin
 				o <= intermediate[4095:0];
 				stage <= 'b0;
 			end

e8a5fd55092197b68806a4f56106009a50741b42
Tim Paine <t.paine154@gmail.com>
2015-02-10 19:31:32 -0500

    moved to multiplier.

--- v2/multiplier/multiplier.sv
+++ v2/multiplier/multiplier.sv
@@ -1,16 +1,19 @@
 `timescale 1ns/1ns
 
 /* module for performing 4096 bit multiplication */
-module modexp(
+module multiplier(
 	clk, rst, a, b, o
 );
 	input logic clk, rst;
-	input logic [4095:0] a, b; //would actually want to chunk this
+	input logic [4095:0] a, b; 
 	output logic [4095:0] o;
 
 	always_ff @(posedge clk) begin
-
-
+		if(rst) begin
+			o <= 'b0;
+		end else begin
+			o <= a*b;
+		end
 	end
 
 endmodule
